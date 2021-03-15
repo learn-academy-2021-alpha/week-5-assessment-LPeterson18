67d58e8aa9bf78b6e1ecd45831b831c9ee64f1b4
LPeterson18 <lpeterson@chorus.net>
2021-03-14 21:21:43 -0700

    All Challenges Complete

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -22,14 +22,49 @@ var secretCodeWord1 = "lackadaisical"
 var secretCodeWord2 = "gobbledygook"
 // Expected output: "g0bbl3dyg00k"
 
-
-
-
+describe("When encodeWord is called", () => {
+  it("return string with 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0. when a string is called",() => {
+    //arrange
+    const argument = secretCodeWord1
+    //act
+    const actualResult = encodeWord(argument)
+    //assert
+  expect(actualResult).toEqual("l4ck4d41s1c4l")
+  })
+  it("return string with 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0. when a string is called",() => {
+    //arrange
+    const argument = secretCodeWord2
+    //act
+    const actualResult = encodeWord(argument)
+    //assert
+  expect(actualResult).toEqual("g0bbl3dyg00k")
+  })
+})
 
 // b) Create the function that makes the test pass.
 
+//pseudocode
+//create a function that takes a string as an argument
+//.split the word into array
+//use .map to cycle through array of letters
+//use if else to check if contains letters and then changes them
 
-
+const encodeWord = (string) => {
+  letterArray = string.toLowerCase().split("")
+  return letterArray.map(letter => {
+    if(letter === "a"){
+    return 4
+    } else if(letter === "e"){
+    return 3
+    } else if(letter === "i"){
+    return 1
+    } else if(letter === "o"){
+    return 0
+    }else{
+    return letter
+    }
+  }).join("")
+}
 
 
 // --------------------2) Create a function that takes in an array and returns all the words that contain the letter a.
@@ -37,18 +72,37 @@ var secretCodeWord2 = "gobbledygook"
 // a) Create a test with an expect statement using the variable provided.
 
 var arrayOfWords = ["Apple", "Banana", "Plum", "Cherry", "Kiwi", "Peach"]
-// Expected output: "Apple" "Banana" "Peach"
+// Expected output: ["Apple" "Banana" "Peach"]
 
-
+describe("When aWordReturner is called", () => {
+  it("return array with all strings with letter a",() => {
+    //arrange
+    const argument = arrayOfWords
+    //act
+    const actualResult = aWordReturner(argument)
+    //assert
+  expect(actualResult).toEqual(["Apple", "Banana", "Peach"])
+  })
+})
 
 
 
 // b) Create the function that makes the test pass.
 
+//pseudocode
+//create function that takes in array as argument
+//.filter to cycle through elements in array and filter out words with letter a
+//.split each item in array
+//.includes to check if word contains the letter
+//return final array
 
-
-
-
+const aWordReturner = (array) => {
+  return array.filter(string => {
+    if(string.toLowerCase().split("").includes("a") === true){
+      return string
+    }
+  })
+}
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with an expect statement using the variable provided.
@@ -60,8 +114,61 @@ var hand2 = [5, 5, 3, 3, 4]
 var hand3 = [5, 5, 5, 5, 4]
 // Expected output: false
 
-
-
-
+describe("When fullHouseChecker is called", () => {
+  it("return true, when an array of numbers is a full house",() => {
+    //arrange
+    const argument = hand1
+    //act
+    const actualResult = fullHouseChecker(argument)
+    //assert
+  expect(actualResult).toEqual(true)
+  })
+  it("return false when an array of number is not a full house",() => {
+    //arrange
+    const argument = hand2
+    //act
+    const actualResult = fullHouseChecker(argument)
+    //assert
+  expect(actualResult).toEqual(false)
+  })
+  it("return false when an array of number is not a full house",() => {
+    //arrange
+    const argument = hand3
+    //act
+    const actualResult = fullHouseChecker(argument)
+    //assert
+  expect(actualResult).toEqual(false)
+  })
+})
 
 // b) Create the function that makes the test pass.
+
+//pseudocode
+//create function that takes array as argument
+//.sort array in order, to create only two win conditions: [a,a,a,b,b] and [a,a,b,b,b]
+// use if else to check if meets either of these conditions
+//return true or false accordingly
+
+// const fullHouseChecker = (array) => {
+//   let sortedArray = array.sort((a,b) => (a-b))
+//   let [a,b,c,d,e] = sortedArray
+//   if(a === c && d === e && a !== e){
+//     return true
+//   }else if(a === b && c === e && a !== e){
+//     return true
+//   }else{
+//     return false
+//   }
+// }
+//
+
+
+//short method
+
+const fullHouseChecker = (array) => {
+  let sortedArray = array.sort((a,b) => (a-b))
+  let [a,b,c,d,e] = sortedArray
+  return ((a === c && d === e && a !== e) || (a === b && c === e && a !== e)) ? true:false
+}
+
+//
--- interview-questions.md
+++ interview-questions.md
@@ -8,63 +8,74 @@ Then, research the question to expand on your answer. Even if you feel you have 
 1. Rails follows an MVC pattern. What does that mean?
 
   Your answer:
+    MVC stands for model, view, controller.  This is a pattern of design, where the user uses the controller, the controller updates the model, the model changes the view, and the user sees the new view.
 
   Researched answer:
-
+  MVC is a software design pattern that stands for Model-View-Controller.  In a MVC pattern, the User uses the Controller, the Controller manipulates the Model, the Model updates the View, and the User sees the newly updated View.
 
 
 2. What does it mean if an application has CRUD? What does CRUD stand for?
 
   Your answer:
+  CRUD represents the basic actions that can be done on a database.  It stands for Create, Read, Update, and Delete.
 
   Researched answer:
-
+  CRUD stands for the four basic operations of persistent storage, these operations being Create, Read, Update, and Delete.
 
 
 3. What is a relational database? Are there other kinds of databases?
 
   Your answer:
+  A relational database is a type of database that is laid out like a table with rows and columns.
 
   Researched answer:
-
+  A relational database contains a collection of data items with predefined relationships between them.  These relationships are used to arrange the data in a table with rows and columns.
 
 
 4. What are the 5 HTTP verbs? What are they important?
 
   Your answer:
+  HTTP is a form of internet security through encryption.
 
   Researched answer:
-
+  HTTP stands for Hypertext Transfer Protocol, and is used for communication between web browsers and web servers.  The HTTP verbs represent request methods that determine how a web server should respond to a particular request.  The five basic verbs are get, post, put, patch, and delete.  Each verb corresponds with a word in the CRUD acronym.
 
 
 5. What is object-relational mapping?
 
   Your answer:
+  Object-relational mapping deals with the relationships between different object, used in a database setting.
 
   Researched answer:
-
+  Object-Relational Mapping or ORM, is used to query and manipulate data from a database using Object-Oriented Programming approach.  It is a library that allows you to use a programming language you are familiar with to manipulate the data in the database.  It converts data between incompatible type systems using object-oriented programming languages.
 
 
 6. What is a gem?
 
   Your answer:
+  A gem is used to download and install different applications to Ruby.
 
   Researched answer:
-
+  gems are Ruby programs and libraries that can be downloaded and installed using the command line tool gem.  A gem itself is a unique self-contained package of data.
 
 
 7. What are primary keys? Why are they important?
 
   Your answer:
+  Primary keys are the ids of different objects in a database.  They are important in that they are unique to the object they are assigned to and they allow you to easily access any specific object in the database quickly.
 
   Researched answer:
-
+  A primary key and is a unique identifier of each instance in a database.  Allow the programmer to easily access that instance.
 
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 - RESTful routes
+  Representation State Transfer is an architectural style used in defining HTTP routes. Routes are the code that are responsible for listening and receiving requests and then deciding what to send back.
 - JSON
+  JSON is a file format that uses text to store and transmit data objects consisting of attribute–value pairs and array data types.
 - Validations
+  Validations are automatically run whenever data is added to a database.  It checks if the data is valid, and if the data returns any errors, it is not saved.
 - Strong params
+  Parameters or params are used by the Rails server to dynamically alter the view according to information input by the user.  Params store information in the url.
